2d73fa3a442412d36a958d6cea3a7a9709aa4e43
Neil Chulani <29507305+TnnsBeast@users.noreply.github.com>
2022-06-13 16:03:51 -0700

    Add files via upload

--- Ball Bin Level Arduino/sketch_jun09b.ino
+++ Ball Bin Level Arduino/sketch_jun09b.ino
@@ -1,11 +1,15 @@
+#include <ros.h>
+
 int adc = A0;
 int distance_cm;
-int bin_max = 4;
 int acceptorState;
 unsigned long mark;
 bool full = false;
 
-
+// sensor range is 4-30cm
+// 30+ cm means no balls (sees black)
+// < 20 cm means bin has some balls (sees white)
+// 4 cm means bin is full
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,21 +18,23 @@ void setup() {
   while (!full) {
     float volts = analogRead(adc)*0.0048828125;
     distance_cm = 13 * pow(volts, -1);
-    if (distance_cm < 40) {
-      Serial.println(distance_cm);
-    } else {
-      Serial.println("40");
-    }
+
+    if (distance_cm >= 30) {
+      //publish no balls
+    } else if (distance > 4) {
+      //publish has some balls
+    } 
+    
     switch(acceptorState) {
       case 0:
-        if (distance_cm <= bin_max) {
+        if (distance_cm <= 4) {
           acceptorState = 1;
           mark = millis();
         }
         break;
       case 1:
         if (millis() - mark > 3000) acceptorState = 2;
-        if (distance_cm > bin_max) acceptorState = 0;
+        if (distance_cm > 4) acceptorState = 0;
         break;
       case 2:
         Serial.println("Full");
